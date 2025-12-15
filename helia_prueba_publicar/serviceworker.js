107979aa544c1f8758da0aad51bbe5099b43b32f
tebicap <tebicap@gmail.com>
2025-12-14 21:16:20 -0300

    agrego type module a scripts.js

--- helia_prueba_publicar/serviceworker.js
+++ helia_prueba_publicar/serviceworker.js
@@ -1,4 +1,4 @@
-CACHE_NAME = 'v5'; // actualizar nro cada vez que actualizo otros archivos para que los recargue
+CACHE_NAME = 'v6'; // actualizar nro cada vez que actualizo otros archivos para que los recargue
 
 const ASSETS = [
         './',
